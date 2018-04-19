8e0bc249ac5ed765272e57bff33f56e89627138b
Moacyr Rodrigues Pereira <moacyr@m3sistemas.com.br>
2018-04-19 12:02:03 -0300

    Inicial

---
+++ CadastroEpi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CadastroEpi.Startup))]
+namespace CadastroEpi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
